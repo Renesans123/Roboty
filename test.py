be1dcf98f12f82a305a0087e6ba8ebe95eb87b49
PatrykZdziech <50749577+Renesans123@users.noreply.github.com>
2025-04-28 11:12:51 +0200

    Update test.py

--- test.py
+++ test.py
@@ -6,119 +6,210 @@ from ev3dev2.led import Leds
 from time import sleep, time
 
 
-def dist(a,b):
-    ret = 0
-    for i in range(len(a)):
-        ret += (a[i]-b[i]) * (a[i]-b[i])
-    return ret
 
 RTIME_90 = 1
 RTIME_180 = 1.7
-
-ts = TouchSensor(INPUT_1)
 leds = Leds()
-sensor1 = ColorSensor(INPUT_2)
-sensor2 = ColorSensor(INPUT_3)
+
+touch_sensor = TouchSensor(INPUT_1)
+color_sensor1 = ColorSensor(INPUT_2)
+color_sensor2 = ColorSensor(INPUT_3)
 left_wheel = LargeMotor(OUTPUT_A)
 right_wheel = LargeMotor(OUTPUT_B)
+# lift = MediumMotor(OUTPUT_C)
 
 # leds.set_color("RIGHT", "GREEN")
 
-# def get_color(sensor):
-#     r, g, b = sensor.rgb
-#     if r > 100 and g > 100 and b > 100:
-#         return 'White'
-#     elif r < 40 and g < 80 and b > 80: #45, 90, 200
-#         # print("\n Blue r: {}, g: {}, b: {}".format(r, g, b))
-#         return 'Blue'
-#     elif r > 50 and g < 40 and b < 40:
-#         # print("\n Green r: {}, g: {}, b: {}".format(r, g, b))
-#         return 'Green'
-#     elif r < 100 and g < 100 and b < 100:
-#         return 'Black'
-#     elif r < 120 and g < 120 and b < 120:
-#         return 'Gray'
-#     else:
-#         return 'White'
-
 def get_color(sensor):
-    # sensor.rgb
-    col = {'WHITE':[255,255,255],'BLACK':[35,35,30],'BLUE':[45, 90, 190], 'GREEN':[40, 115, 60],'YELLOW':[255, 175, 60],'RED':[255, 40, 30]}
-    min_d = 9999999999
-    
-    for c in ['WHITE','BLACK','BLUE', 'GREEN','YELLOW','RED']:
-        d = dist(col[c],sensor.rgb)
-        min_d = min(d,min_d)
-        col[d] = c
-    return col[min_d]
-    
-#green 40 115 60
-#yellow 255 175 60
-#red 255 40 30
-
-
-def rot_90(speed, left=True):
-    if left:
-        left_wheel.on(speed*2)
-        right_wheel.on(-speed)
-        sleep(RTIME_90)
+    r, g, b = sensor.rgb
+    if r > 100 and g > 100 and b > 100:
+        return 'WHITE'
+    elif r < 70 and g < 120 and b > 100: #45, 90, 200
+        # print("\n Blue r: {}, g: {}, b: {}".format(r, g, b))
+        return 'BLUE'
+    elif r > 50 and g < 40 and b < 40:
+        # print("\n Green r: {}, g: {}, b: {}".format(r, g, b))
+        return 'GREN'
+    elif r < 100 and g < 100 and b < 100:
+        return 'BLACK'
     else:
-        left_wheel.on(-speed)
-        right_wheel.on(speed*2)
-        sleep(RTIME_90)
-        left_wheel.stop()
-        right_wheel.stop()
+        return 'WHITE'
 
-def rot_180(speed):
-    left_wheel.on(speed*2)
-    right_wheel.on(-speed*2)
-    sleep(RTIME_180)
 
-def forward(speed):
-    left_wheel.on(speed)
-    right_wheel.on(speed)
 
-def backward(speed):
-    left_wheel.on(-speed)
-    right_wheel.on(-speed)
+# def rot_90(speed, left=True):
+#     if left:
+#         left_wheel.on(speed*2)
+#         right_wheel.on(-speed)
+#         sleep(RTIME_90)
+#     else:
+#         left_wheel.on(-speed)
+#         right_wheel.on(speed*2)
+#         sleep(RTIME_90)
+#         left_wheel.stop()
+#         right_wheel.stop()
 
-def turn(speed, dir):
-    if dir >0:
-        left_wheel.on(speed * dir)
-        right_wheel.on(speed)
+# def rot_180(speed):
+#     left_wheel.on(speed*2)
+#     right_wheel.on(-speed*2)
+#     sleep(RTIME_180)
+
+# def forward(speed):
+#     left_wheel.on(speed)
+#     right_wheel.on(speed)
+
+# def backward(speed):
+#     left_wheel.on(-speed)
+#     right_wheel.on(-speed)
+
+# def turn2(speed, direction):
+#     if direction >0:
+#         left_wheel.on(speed * direction)
+#         right_wheel.on(speed)
+#         # right_wheel.on(speed * -direction)
+#     else:
+#         direction = -direction
+#         # left_wheel.on(speed * direction)
+#         left_wheel.on(speed) 
+#         right_wheel.on(speed* direction)
+
+# def turn(speed,direction):
+#     w1, w2 =left_wheel,right_wheel
+#     if direction < 0:
+#         w1 = right_wheel
+#         w2 = left_wheel
+#         direction = -direction
+#     if direction>0.2:
+#         w1.on(speed)
+#         w2.on(speed)
+
+# def rot_90(speed, left=True):
+#     if left:
+#         left_wheel.on(speed*2)
+#         right_wheel.on(-speed)
+#         sleep(RTIME_90)
+#     else:
+#         left_wheel.on(-speed)
+#         right_wheel.on(speed*2)
+#         sleep(RTIME_90)
+#         left_wheel.stop()
+#         right_wheel.stop()
+
+# def rot_180(speed):
+#     left_wheel.on(speed*2)
+#     right_wheel.on(-speed*2)
+#     sleep(RTIME_180)
+
+# def forward(speed):
+#     left_wheel.on(speed)
+#     right_wheel.on(speed)
+
+# def backward(speed):
+#     left_wheel.on(-speed)
+#     right_wheel.on(-speed)
+
+# def turn2(speed, direction):
+#     if direction >0:
+#         left_wheel.on(speed * direction)
+#         right_wheel.on(speed)
+#         # right_wheel.on(speed * -direction)
+#     else:
+#         direction = -direction
+#         # left_wheel.on(speed * direction)
+#         left_wheel.on(speed) 
+#         right_wheel.on(speed* direction)
+
+# def turn(speed,direction):
+#     w1, w2 =left_wheel,right_wheel
+#     if direction < 0:
+#         w1 = right_wheel
+#         w2 = left_wheel
+#         direction = -direction
+#     if direction>0.2:
+#         w1.on(speed)
+#         w2.on(speed)
+#     else:
+#         w1.on(speed * direction)
+#         w2.on(speed)
+#     else:
+#         w1.on(speed * direction)
+#         w2.on(speed)
+
+def go(speed, direction):
+    w1, w2 =left_wheel,right_wheel
+    if speed == 0:
+        w1.stop()
+        w2.stop()
+    elif direction < 0:
+        w1 = right_wheel
+        w2 = left_wheel
+        direction = -direction
+    elif direction < 1:
+        w1.on(speed - speed * direction)
+        w2.on(speed + speed * direction)
     else:
-        left_wheel.on(speed * -dir) 
-        right_wheel.on(speed)
+        w1.on(-speed * direction)
+        w2.on(+ speed * direction)
+
 
 try:
     temp_time = time()
 
+    speed = 10
+    direction = 0
     while True:
-        color = get_color(sensor1)
-        if color == 'BLUE':
-            # leds.brightness(0)
-            pass
-        elif color == 'WHITE':
-            # leds.brightness(99)
-            pass
-        else:
-            leds.set_color("RIGHT", color)
+        # color = get_color(color_sensor1)
+        colorL = get_color(color_sensor1)
+        colorR = get_color(color_sensor2)
+
+        # full throttle
+        if colorL == colorR == 'WHITE':
+            speed = 10
+            direction = 0
+        elif colorL == colorR == "BLACK":
+            speed = 5
+            direction = 1
+        elif colorL == "BLACK":
+            speed = 10
+            direction = max(1, direction+0.1)
+        elif colorL == colorR == "BLUE":
+            speed = 0
+            direction = 0
+            go(speed, direction)
+            sleep(1)
+            
+        go(speed, direction)
+
+        print("left/right :" + colorL + '/' + colorR + " timeD:" + str(time() - temp_time))
+        # "RGB" + str(color_sensor1.rgb) + 
+        
+
+        # direction = color_sensor1.reflected_light_intensity - 50
+        # direction*=0.01
+        # direction = max(-1,min(direction,1))
+
+        #leds.set_color("RIGHT", color)
         # print("1")
-        print("RGB" + str(sensor1.rgb) + "col:" 
-        + color 
-        + "    timeD:" + str(time() - temp_time))
-        dir = sensor1.reflected_light_intensity - 35
-        turn(15,dir*2)
+        # + " direction: " + str(direction)
+        # turn(10,direction)
+        # if color == 'BLACK':
+        #     turn(5,0.5)
+        # else:
+        #     turn(5,-0.5)
         
         temp_time = time()
 
-        if ts.is_pressed:
+        if touch_sensor.is_pressed:
             left_wheel.stop()
             right_wheel.stop()
             raise KeyboardInterrupt
 except KeyboardInterrupt:
     left_wheel.stop()
     right_wheel.stop()
+except BaseException as e:
+    left_wheel.stop()
+    right_wheel.stop()
+    print("Błąd b :", e)
 except Exception as e:
     left_wheel.stop()
     right_wheel.stop()
