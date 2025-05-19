028320aa9ad3a488c6521b4619ab5135c27365cf
PatrykZdziech <50749577+Renesans123@users.noreply.github.com>
2025-05-19 10:06:51 +0200

    notesa dded

--- stop.py
+++ stop.py
@@ -8,3 +8,19 @@ left_wheel.stop()
 right_wheel.stop()
 
 
+# def get_color(sensor):
+#     # sensor.rgb
+#     col = {'WHITE':[255,255,255],'BLACK':[35,35,30],'BLUE':[45, 90, 190], 'GREEN':[40, 115, 60],'YELLOW':[255, 175, 60],'RED':[255, 40, 30]}
+#     min_d = 9999999999
+    
+#     for c in ['WHITE','BLACK','BLUE', 'GREEN','YELLOW','RED']:
+#         d = dist(col[c],sensor.rgb)
+#         min_d = min(d,min_d)
+#         col[d] = c
+#     return col[min_d]
+    
+#green 40 115 60
+#yellow 255 175 60
+#red 255 40 30
+
+#scp ./fastpz.py robot@192.168.18.80:~/fastpz.py
